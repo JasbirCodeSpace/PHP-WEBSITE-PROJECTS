7175c13c46054af00f51adb8dd96cd15f1573041
Jasbir <shikhawat.jasbir@gmail.com>
2020-01-11 18:40:42 +0530

    Create checks.js

---
+++ Restaurant Website/js/checks.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+  function disablePaste(obj){
+    var newObj = null;
+    if(typeof(obj) == "object") newObj = obj;
+    else if(typeof(obj) == "string") newObj = document.getElementById(obj);
+    newObj.onpaste = function(e){
+      e.preventDefault();
+    }
+  }
+
+  function enablePaste(obj){
+    var newObj = null;
+    if(typeof(obj) == "object") newObj = obj;
+    else if(typeof(obj) == "string") newObj = document.getElementById(obj);
+    newObj.onpaste = function(e){
+      return true;
+    }
+  }
+  var idsCopyPaste = ['credt_abb','lastnetprofit'];
+  for (var i = 0; i < idsCopyPaste.length; i++) {
+    disablePaste(document.getElementById(idsCopyPaste[i]));
+  }
+
+
+  function isNumberKey(evt)
+  {
+     var charCode = (evt.which) ? evt.which : event.keyCode
+     if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+
+     return true;
+  }
+
+  function isAlphabetKey(evt){
+    var charCode = (evt.which) ?evt.which :evt.keyCode;
+    if(((charCode >= 65 && charCode <=90) ||((charCode >= 97 && charCode <=122))))
+    return true;
+
+    return false;
+  }
+
+  function isDecimalNumber(evt,obj){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    var value = obj.value;
+    var dotcontains = value.indexOf(".") != -1;
+    if (dotcontains){
+        if (charCode == 46) return false;
+        if(value.length==value.indexOf('.')+3) return false;
+      }
+    if (charCode == 46) return true;
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+  }
+
+
+  function isLimitedLength(evt,obj,type,len){
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    var value = obj.value;
+
+  if(value.length>=len) return false;
+
+  var status = false;
+  switch (type) {
+    case "integer":
+                    status = isNumberKey(evt);
+                    break;
+
+    case "decimal":
+                    status = isDecimalNumber(evt,obj);
+                    break;
+    case "character":
+                    status = isAlphabetKey(evt);
+                    break;
+    default:
+                    status = false;
+                    break;
+  }
+
+  return status;
+
+  }
+
+
+})
