83b929e30768c9bd43244963f9018b1af4339a35
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-02 14:10:54 +0530

    Added PHP_MAILER files and config file

---
+++ New_Product_Campaign/php_mailer/config.php
@@ -0,0 +1,18 @@
+<?php
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth = "true";
+$mail->SMTPSecure = "tls";
+$mail->Port = "587";
+$mail->Username = "shikhawat.jasbir@gmail.com";
+$mail->Password = "password";
+?>
