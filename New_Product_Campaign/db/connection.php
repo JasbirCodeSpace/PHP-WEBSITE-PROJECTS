b585cefef1b1f119f34dbdc8b28483abe56d1a9d
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-02 13:31:36 +0530

    Updated db Connection file

--- New_Product_Campaign/db/connection.php
+++ New_Product_Campaign/db/connection.php
@@ -6,4 +6,9 @@ $password = '';
 $db = 'honeymint';
 
 $con = mysqli_connect($host,$user,$password,$db);
+
+if($con->connect_errno){
+	echo json_encode(array('status'=>false,'msg'=>'DB connection error'));
+	exit;
+}
 ?>
