6e0b436365a16eeacd8da59654a8a6b470d7be66
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-03 19:33:30 +0530

    Added encrypt decrypt methods

--- New_Product_Campaign/db/encrypt_decrypt.php
+++ New_Product_Campaign/db/encrypt_decrypt.php
@@ -1,19 +1,24 @@
 <?php
-// $encryption_key_256bit = base64_encode(openssl_random_pseudo_bytes(32));
-$key = "tX5fscLZkqb+i5cR/79KG/uOv4vN17HdIcTyuvLFZ3s=";
+error_reporting(0);
+$key = "#H#O#N#E#Y#M#I#N#T#";
+$ENCRYPTION_KEY = '#H#O#N#E#Y#M#I#N#T#';
+$ENCRYPTION_ALGORITHM = 'AES-256-CBC';
 
 
-function my_encrypt($data,) {
-    $encryption_key = base64_decode($key);
-    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));.
-    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
-
-    return base64_encode($encrypted . '::' . $iv);
+function my_encrypt($data) {
+    global $ENCRYPTION_KEY;
+    global $ENCRYPTION_ALGORITHM;
+    $EncryptionKey = base64_decode($ENCRYPTION_KEY);
+    $InitializationVector  = openssl_random_pseudo_bytes(openssl_cipher_iv_length($ENCRYPTION_ALGORITHM));
+    $EncryptedText = openssl_encrypt($data, $ENCRYPTION_ALGORITHM, $EncryptionKey, 0, $InitializationVector);
+    return base64_encode($EncryptedText . '::' . $InitializationVector);
 }
 
 function my_decrypt($data) {
-    $encryption_key = base64_decode($key);
-    list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
-    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+global $ENCRYPTION_KEY;
+    global $ENCRYPTION_ALGORITHM;
+    $EncryptionKey = base64_decode($ENCRYPTION_KEY);
+    list($Encrypted_Data, $InitializationVector ) = array_pad(explode('::', base64_decode($data), 2), 2, null);
+    return openssl_decrypt($Encrypted_Data, $ENCRYPTION_ALGORITHM, $EncryptionKey, 0, $InitializationVector);
 }
 ?>
