bbf2fb19a04699a212a9f2eededc35ba20fbc7fe
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-03 19:34:04 +0530

    Added user registration logic

--- New_Product_Campaign/db/register.php
+++ New_Product_Campaign/db/register.php
@@ -1,6 +1,9 @@
 <?php
-require('connection.php');
+session_start();
+error_reporting(0);
+require ('connection.php');
 require 'encrypt_decrypt.php';
+require '../sendMail.php';
 
 $data = $_POST['formData'];
 $fname = $con->real_escape_string($data['fname']);
@@ -10,25 +13,49 @@ $phone = $con->real_escape_string($data['phone']);
 
 $query = "SELECT * FROM `user` WHERE email = '$email'";
 $result = $con->query($query);
-if (!$result) {
-	$sharelink = my_encrypt($email);
-$query = "INSERT INTO `user`(firstName,lastName,email,phone,password) VALUES('$fname','$lname','$email','$phone','$password')";
-$result = $con->query($query);
-$json = '';
-if ($result) {
-	$json = array('status'=>true,'msg'=>'Successfully registered');
+if (mysqli_num_rows($result) == 0)
+{
+    $sharelink = my_encrypt(addslashes($email));
+    $query = "INSERT INTO `user`(firstName,lastName,email,phone,link) VALUES('$fname','$lname','$email','$phone','$sharelink')";
+    $result = $con->query($query);
+    $json = '';
+    if ($result)
+    {
+        $data = array(
+            'name' => $fname,
+            'link' => $sharelink,
+            'email' => $email
+        );
+        $emailResponse = sendMail($data, 'register');
 
-	$query = "UPDATE `user` SET count = count + 1 WHERE id = {$_SESSION['refferd_id']}";
-	$con->query($query);
-}else{
-	$json =  array('status'=>false,'msg'=>"Error while registration");
+        $json = array(
+            'status' => true,
+            'msg' => 'Successfully registered'
+        );
+
+        if ($_SESSION['reffered_id'] != '')
+        {
+            $query = "UPDATE `user` SET count = count + 1 WHERE id = {$_SESSION['reffered_id']}";
+            $result = $con->query($query);
+        }
+    }
+    else
+    {
+        $json = array(
+            'status' => false,
+            'msg' => "Error while registration"
+        );
+    }
 }
-}else{
-	$json = json_encode(array('status'=>false,'msg'=>'Given email already registered'));
+else
+{
+    $json = json_encode(array(
+        'status' => false,
+        'msg' => 'Given email already registered'
+    ));
 }
 
-
-$con->close();;
+$con->close();
 echo json_encode($json);
 exit;
 
