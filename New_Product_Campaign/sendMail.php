4b7c81ed55892a8accfc890f7ebbabee2ce3cdff
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-03 19:35:22 +0530

    Added sendMail logic after successfull registration

--- New_Product_Campaign/sendMail.php
+++ New_Product_Campaign/sendMail.php
@@ -1,13 +1,31 @@
 <?php
-require 'php_mailer/config.php';
-$mail->Subject = "Test Mail";
-$mail->setFrom("shikhawat.jasbir@gmail.com");
-$mail->Body="Hi there";
-$mail->addAddress("shikhawat.jasbir@gmail.com");
-if ($mail->Send()) {
-	echo json_encode(array("status"=>true,"msg"=>"Sent Successfully"));
-}else{
-	echo json_encode(array("status"=>false,"msg"=>"Error while sending mail"));
-}
 
+require 'php_mailer/config.php';
+
+function sendMail($data,$action){
+global $mail;
+if($action=='register'){
+$mail->Subject = "Successfully registered for HoneyMint Contest";
+$file=file_get_contents('emailTemplate.php');
+$link = $_SESSION['honeymint_home'];
+$file= str_replace(array("{name}","{link}"),array($data['name'],$link.'?id='.$data['link']), $file);
+$mail->Body = $file;
+}elseif ($action=="winner") {
+$mail->Subject = "Congratulation on winning HoneyMint Contest";
+$file=file_get_contents("winnerEmailTemplate.php");
+$link = isset($_SESSION['honeymint_home'])?$_SESSION['honeymint_home']:"http://localhost/PHP-WEBSITE-PROJECTS/New_Product_Campaign";
+$file= str_replace(array("{name}","{link}"),array($data['name'],$link), $file);
+$mail->Body = $file;
+}else{exit;}
+$mail->setFrom("contact@honemint.com","HoneyMint");
+
+$mail->addAddress(trim($data['email']));
+$mail->isHTML(true);
+
+if ($mail->Send()) {
+	return json_encode(array("status"=>true,"msg"=>"Sent Successfully"));
+}else{
+	return json_encode(array("status"=>false,"msg"=>"Error while sending mail"));
+}
+}
 ?>
