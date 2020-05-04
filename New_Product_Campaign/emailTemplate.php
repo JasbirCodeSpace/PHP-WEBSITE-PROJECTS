e205fb07fd1c992f3c14ee98545975804b7a7cc7
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-04 12:25:54 +0530

    Added successfull registration email template

---
+++ New_Product_Campaign/emailTemplate.php
@@ -0,0 +1,62 @@
+<div id="fb-root">&nbsp;</div>
+<table class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;" border="0" cellspacing="0" cellpadding="0">
+    <tbody>
+        <tr>
+            <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
+            <td class="container" style="font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;">
+                <div class="content" style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px;">
+                    <table class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff; border-radius: 3px;">
+                        <!-- START MAIN CONTENT AREA -->
+                        <tbody>
+                            <tr>
+                                <td class="wrapper" style="font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;">
+                                    <table style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                            <tr>
+                                                <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">
+                                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Hi {name},</p>
+                                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Congratulation for registering for HoneyMint Contest.</p>
+                                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">You are one step closer in winning 100 bottles of our new HoneyMint. Share below link to increase your chances to win</p>
+                                                    <table class="btn btn-primary" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;" border="0" cellspacing="0" cellpadding="0">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;" align="left">
+                                                                    <table style="border-collapse: separate; width: 261px;" border="0" cellspacing="0" cellpadding="0">
+                                                                        <tbody>
+                                                                            <tr style="height: 47px;">
+                                                                                <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center; width: 124px; height: 47px;"><a style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;" href="{link}" target="_blank" rel="noopener">Share link</a></td>
+                                                                            </tr>
+                                                                            <tr>
+                                                                                <td style="padding-left: 30px">
+                                                                                    OR
+                                                                                </td>
+                                                                            </tr>
+                                                                            <tr style="height: 18px;">
+                                                                                <td style="width: 124px; height: 18px;">
+                                                                                    <div class="fb-share-button" data-href="https://www.epochconverter.com/" data-layout="button" data-size="large"><a class="fb-xfbml-parse-ignore" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;" href="{link}" target="_blank" rel="noopener">facebook</a></div>
+                                                                                </td>
+                                                                                <td style="width: 131px; height: 18px;"><a class="twitter-share-button" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;" href="{link}" target="_blank" rel="noopener" data-size="large" data-text="Join HoneyMint contest and win 100 bottles of HoneyMint" data-url="{link}" data-show-count="false">Tweet</a></td>
+                                                                            </tr>
+                                                                        </tbody>
+                                                                    </table>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <p style="font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 15px;">Good luck!</p>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            <!-- END MAIN CONTENT AREA -->
+                        </tbody>
+                    </table>
+                    <!-- END CENTERED WHITE CONTAINER -->
+                </div>
+            </td>
+            <td style="font-family: sans-serif; font-size: 14px; vertical-align: top;">&nbsp;</td>
+        </tr>
+    </tbody>
+</table>
